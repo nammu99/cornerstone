c66e7b62c1f89ab9f731897240e9e1ad30aebf3c
nammu99 <120698706+nammu99@users.noreply.github.com>
2023-03-09 21:26:52 -0600

    Create main.tf

---
+++ terraform-jenkins/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami             = "ami-060d3509162bcc386"
+  instance_type   = "t2.micro"
+  key_name        = "nameera"
+  security_groups = ["default"]
+  user_data = file("jenkins.sh")
+
+  tags = {
+    Name = "Jenkins_server-terraform"
+  }
+}
+  resource "aws_security_group" "default" {
+	tags = {
+	type = "jenkins-security-group"
+}
+}
