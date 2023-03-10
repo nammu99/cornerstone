4da2e201f7eb0c1d583897b0939837c613ca15a1
nammu99 <120698706+nammu99@users.noreply.github.com>
2023-03-09 21:27:37 -0600

    Create jenkins.sh

---
+++ terraform-jenkins/jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum install ec2-instance-connect
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo yum install git -y
